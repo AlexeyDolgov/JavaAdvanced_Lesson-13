f46ce915a7c47044f596d9c191a483cfa652020f
Alexey Dolgov <bifourcator@gmail.com>
2019-09-24 16:39:16 +0300

    Java Advanced Lesson 13 done!

---
+++ JavaAdvanced_Lesson-13/src/main/java/ua/lviv/lgs/magazineShop/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package ua.lviv.lgs.magazineShop.dao;
+
+import ua.lviv.lgs.magazineShop.domain.User;
+
+public interface UserDAO extends DAOAbstractCRUD<User>{
+
+	User readByEmail(String email) throws DAOException;	
+	
+}
